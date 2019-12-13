a2f5769ffe4d6db61e19c8341f183181ff2150bd
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2019-12-13 15:27:51 +0800

    no message

--- src/Capinfo.Core/Configuration/AppSettingNames.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Capinfo.Configuration
-{
-    public static class AppSettingNames
-    {
-        public const string UiTheme = "App.UiTheme";
-    }
-}
--- src/Capinfo.Web.Core/Authentication/External/IExternalAuthConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Capinfo.Authentication.External
-{
-    public interface IExternalAuthConfiguration
-    {
-        List<ExternalLoginProviderInfo> Providers { get; }
-    }
-}
