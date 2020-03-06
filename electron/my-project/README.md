9a7486cf55e5ea06a713a3daa172cbbab4369b8b
wuhailong <13126506430@163.com>
2020-03-06 15:56:14 +0800

    1.添加electron，360，火绒会导致程序启动缓慢 2.使用cnpm run package 打包

---
+++ electron/my-project/README.md
@@ -0,0 +1,31 @@
+# my-project
+
+> An electron-vue project
+
+#### Build Setup
+
+``` bash
+# install dependencies
+cnpm install
+
+# serve with hot reload at localhost:9080
+cnpm run dev
+
+# build electron application for production
+cnpm run build
+
+# run unit & end-to-end tests
+cnpm test
+
+# 程序打包
+cnpm run package
+
+
+# lint all JS/Vue component files in `src/`
+cnpm run lint
+
+```
+
+---
+
+This project was generated with [electron-vue](https://github.com/SimulatedGREG/electron-vue) using [vue-cli](https://github.com/vuejs/vue-cli). Documentation about the original structure can be found [here](https://simulatedgreg.gitbooks.io/electron-vue/content/index.html).
---
+++ electron/my-project/src/main/index.js
@@ -0,0 +1,77 @@
+'use strict'
+
+import { app, BrowserWindow } from 'electron'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+// const winURL = process.env.NODE_ENV === 'development'
+// ? `http://localhost:8080`
+// : `file://${__dirname}/index.html`
+
+const electron = require('electron')
+const Menu = electron.Menu
+const path = require('path')
+const renderProcessApi = path.join(__dirname, './inject.js')
+function createWindow() {
+  Menu.setApplicationMenu(null)
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 563,
+    useContentSize: true,
+    width: 1000,
+    minWidth: 1024,
+    minHeight: 768,
+    webPreferences: {
+      preload: renderProcessApi
+    }
+  })
+
+  mainWindow.loadURL('http://localhost:8080')
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
